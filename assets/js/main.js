cb64b2e22e8f644ae501becb8e7cb3699ce342be
nabeel-khanjee <nabeelshakeel966@gmail.com>
2020-11-13 16:14:45 +0000

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
+
+$(document).ready(function() {
+  $('input#input_text, textarea#textarea2').characterCounter();
+});
+      
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
